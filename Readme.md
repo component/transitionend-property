38a1dfed37c9ce28dc2c4fab089d4dc10acd37e9
Matt Mueller <mattmuelle@gmail.com>
2013-07-11 13:12:58 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+
+# transitionend-property
+
+  get the browser-specific transitionend property
+
+## Installation
+
+    $ component install component/transitionend-property
+
+## Example
+
+```js
+var transitionend = require('transitionend-property');
+el.addEventListener(transitionend, fn);
+```
+
+## API
+
+  Exports the name of transform property as a String or null if the property is not supported.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Module Dependencies
+ */
+
+var transform = require('transform-property');
+
+/**
+ * Transition-end mapping
+ */
+
+var map = {
+  'WebkitTransition' : 'webkitTransitionEnd',
+  'MozTransition' : 'transitionend',
+  'OTransition' : 'oTransitionEnd',
+  'msTransition' : 'MSTransitionEnd',
+  'transition' : 'transitionend'
+};
+
+/**
+ * Expose `transitionend`
+ */
+
+var el = document.createElement('p');
+
+for (var transition in map) {
+  if (null != el.style[transition]) {
+    module.exports = map[transition];
+    break;
+  }
+}
